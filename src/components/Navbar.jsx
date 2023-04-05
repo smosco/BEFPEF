28771895e506bd29b722cb3e64478852587d7e14
ore <go1ruf2tk3@gmail.com>
2023-04-05 11:56:32 +0900

    navbar 반응형 구현

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/Navbar.scss";
 import { SiDatadog } from "react-icons/si";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 export default function Navbar() {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen((prev) => !prev);
+  };
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
@@ -17,19 +22,26 @@ export default function Navbar() {
           <SiDatadog className="icon" />
           <h1>펫프</h1>
         </Link>
-        <ul className="links">
-          <li>
+        <ul className={`links ${open ? "open" : "closed"}`}>
+          <li onClick={handleOpen}>
             <Link to="/" onClick={() => window.scrollTo(0, 0)}>
               Home
             </Link>
           </li>
-          <li>
+          <li onClick={handleOpen}>
             <Link to="/location">Location</Link>
           </li>
-          <li>
+          <li onClick={handleOpen}>
             <Link to="/beFriend">Be Friend</Link>
           </li>
         </ul>
+        <div className="menu" onClick={handleOpen}>
+          {open ? (
+            <AiOutlineClose className="icon" />
+          ) : (
+            <AiOutlineMenu className="icon" />
+          )}
+        </div>
       </div>
     </nav>
   );
